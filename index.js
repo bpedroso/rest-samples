38f0152242473e19aec69c5a73af6e00d34d0208
brunopedrosoc4 <bruno_pedroso@carrefour.com>
2017-02-14 01:10:35 -0200

    Add logs and auth

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-var cool = require('cool-ascii-faces');
+//Express APP
 var express = require('express')
 var app = express()
-var bodyParser = require('body-parser')
 var port = 3000
-
 app.set('port', (process.env.PORT || port));
 
+//Auth
+var auth = require('http-auth');
+var basic = auth.basic({
+  realm: "Simon Area.",
+  file: __dirname + "/data/users.htpasswd"
+});
+
+//Scribe LOGS
+var scribe = require('scribe-js')();
+var console = process.console;
+
+var cool = require('cool-ascii-faces');
+var bodyParser = require('body-parser');
+
+app.use(auth.connect(basic));
+app.use('/logs', scribe.webPanel());
+app.use(scribe.express.logger());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 
 app.all('/v1/sample', function (req, res, next) {
   var resBody = JSON.stringify(req.body, null, 2);
   var messageId = req.get('messageId');
 
-  console.log(new Date().toISOString(), ' - messageId: ', messageId);
-  console.log('Receiving: ', resBody);
+  console.info(new Date().toISOString(), ' - messageId: ', messageId);
+  console.info('Receiving: ', resBody);
   next();
 });
 
@@ -27,6 +43,6 @@ app.get('/cool', function(request, response) {
 });
 
 app.listen(port, function () {
-  console.log('Listening on port ', port)
+  console.date().log('Listening on port ', port)
 });
 
